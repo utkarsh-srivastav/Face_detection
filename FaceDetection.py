8350729f0ce23c7abd47891432ead30d234489e7
Utkarsh <utkarshsrivastav233@gmail.com>
2020-09-10 11:10:21 +0530

    Initial commit

---
+++ FaceDetection.py
@@ -0,0 +1,19 @@
+import cv2
+
+# creates cascade classifier object
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# reading img
+img = cv2.imread("C:\\Users\\hp\\Desktop\\DSC_6867.jpg")
+# convert to gray scale img
+# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# search the coord of the img
+# faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.05, minNeighbors=5)
+faces = face_cascade.detectMultiScale(img, scaleFactor=1.05, minNeighbors=5)
+print(type(faces))
+print(faces)
+
+for x, y, w, h in faces:
+    img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 300, 0), 2)
+cv2.imshow("window_name", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
